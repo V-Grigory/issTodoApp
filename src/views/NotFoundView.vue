3dd2552201820cbc57e9b967b10ef90c557316d9
Grigory Volchok <volchok@ntsmail.ru>
2024-04-25 14:31:23 +0500

    Add route. Remove unnecessary assets.

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container class="d-flex flex-column align-center h">
+    <v-row class="text-h1" align="end"> 404 </v-row>
+    <v-row class="text-h5"> Страница не найдена </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+.h {
+  height: 500px;
+}
+</style>
