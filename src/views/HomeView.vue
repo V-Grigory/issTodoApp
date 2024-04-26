3e4c5ca11818d5badd6cc5d53094f69bcd75a1a7
Grigory Volchok <volchok@ntsmail.ru>
2024-04-26 11:39:58 +0500

    Remove unnecessary components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import JobsList from "@/components/widgets/JobsList.vue";
-import AddJob from "@/components/widgets/AddJob.vue";
 
 import Notification from "@/components/ui/Notification.vue";
 </script>
 
 <template>
   <jobs-list />
-  <add-job />
 
   <notification />
 </template>
